b85efd7d2e737165b83b30e67486bb8f3241a18b
Zibeyda Bayramova <zibeyda.bayramova@ateshgah.com>
2022-06-24 10:37:44 +0400

    initial commit

---
+++ calculator.js
@@ -0,0 +1,20 @@
+// Clear all the value
+
+function clearScreen() {
+    document.getElementById("result").value = "";
+}
+
+// display values
+
+function display(value) {
+    document.getElementById("result").value += value;
+
+}
+
+// evaluates the expression and returns result/ оценивает выражение и возвращает результат
+
+function calculate() {
+    var p = document.getElementById("result").value;
+    var q = eval(p);
+    document.getElementById("result").value = q;
+}
